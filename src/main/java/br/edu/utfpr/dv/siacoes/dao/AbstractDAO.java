d90c18292405e387a29318662f3a65fa6608da8a
Gilberto <gilberto.falco@outlook.com>
2020-09-15 15:42:21 -0300

    Criando a classe Abstract

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/AbstractDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/AbstractDAO.java
@@ -4,8 +4,10 @@ import java.sql.SQLException;
 import java.util.List;
 
 public abstract class AbstractDAO<T> {
-	
-	public abstract int save(int id, T entity) throws SQLException;
+
+        public abstract T loadObject(ResultSet rs)  throws SQLException;
+	public abstract int save(int id, T t) throws SQLException;
 	public abstract T findById(int id) throws SQLException;
-	public abstract List<T> listAll() throws SQLException;
+	public abstract List<T> listAll(boolean b) throws SQLException;
+        public abstract List<T> listAll() throws SQLException;
 }
